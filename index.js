432d1a7d418b6809a9ba3be3eba8c83f031d78d7
jonoman55 <jonoman55@yahoo.com>
2021-11-22 13:40:26 -0500

    seperated api and file routes

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.use(cors());
 
 app.use(express.static(__dirname + '/public'));
 
-app.use('/api', require('./routes'));
+app.use('/api', require('./routes/api'));
+app.use('/file', require('./routes/file'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
